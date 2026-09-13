74d70a479563bf4b3d44c7b644fe266d78c63b94
Vitor Amaral <vitor.do.amaral@sap.com>
2026-09-13 18:48:28 -0300

    chore: regenerate route tree after merge with main

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,230 +8,230 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from "./routes/__root";
-import { Route as IndexRouteImport } from "./routes/index";
-import { Route as AdministradorRouteImport } from "./routes/administrador";
-import { Route as CadastroRouteImport } from "./routes/cadastro";
-import { Route as CadastroEmpresaRouteImport } from "./routes/cadastro-empresa";
-import { Route as CandidatoRouteImport } from "./routes/candidato";
-import { Route as EmpresaRouteImport } from "./routes/empresa";
-import { Route as ForgotPasswordRouteImport } from "./routes/forgot-password";
-import { Route as LoginRouteImport } from "./routes/login";
-import { Route as ResetPasswordRouteImport } from "./routes/reset-password";
-import { Route as UsersRegisterRouteImport } from "./routes/users/register";
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as AdministradorRouteImport } from './routes/administrador'
+import { Route as CadastroRouteImport } from './routes/cadastro'
+import { Route as CadastroEmpresaRouteImport } from './routes/cadastro-empresa'
+import { Route as CandidatoRouteImport } from './routes/candidato'
+import { Route as EmpresaRouteImport } from './routes/empresa'
+import { Route as ForgotPasswordRouteImport } from './routes/forgot-password'
+import { Route as LoginRouteImport } from './routes/login'
+import { Route as ResetPasswordRouteImport } from './routes/reset-password'
+import { Route as UsersRegisterRouteImport } from './routes/users/register'
 
 const IndexRoute = IndexRouteImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const AdministradorRoute = AdministradorRouteImport.update({
-  id: "/administrador",
-  path: "/administrador",
+  id: '/administrador',
+  path: '/administrador',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const CadastroRoute = CadastroRouteImport.update({
-  id: "/cadastro",
-  path: "/cadastro",
+  id: '/cadastro',
+  path: '/cadastro',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const CadastroEmpresaRoute = CadastroEmpresaRouteImport.update({
-  id: "/cadastro-empresa",
-  path: "/cadastro-empresa",
+  id: '/cadastro-empresa',
+  path: '/cadastro-empresa',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const CandidatoRoute = CandidatoRouteImport.update({
-  id: "/candidato",
-  path: "/candidato",
+  id: '/candidato',
+  path: '/candidato',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const EmpresaRoute = EmpresaRouteImport.update({
-  id: "/empresa",
-  path: "/empresa",
+  id: '/empresa',
+  path: '/empresa',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const ForgotPasswordRoute = ForgotPasswordRouteImport.update({
-  id: "/forgot-password",
-  path: "/forgot-password",
+  id: '/forgot-password',
+  path: '/forgot-password',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const LoginRoute = LoginRouteImport.update({
-  id: "/login",
-  path: "/login",
+  id: '/login',
+  path: '/login',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const ResetPasswordRoute = ResetPasswordRouteImport.update({
-  id: "/reset-password",
-  path: "/reset-password",
+  id: '/reset-password',
+  path: '/reset-password',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 const UsersRegisterRoute = UsersRegisterRouteImport.update({
-  id: "/users/register",
-  path: "/users/register",
+  id: '/users/register',
+  path: '/users/register',
   getParentRoute: () => rootRouteImport,
-} as any);
+} as any)
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute;
-  "/administrador": typeof AdministradorRoute;
-  "/cadastro": typeof CadastroRoute;
-  "/cadastro-empresa": typeof CadastroEmpresaRoute;
-  "/candidato": typeof CandidatoRoute;
-  "/empresa": typeof EmpresaRoute;
-  "/forgot-password": typeof ForgotPasswordRoute;
-  "/login": typeof LoginRoute;
-  "/reset-password": typeof ResetPasswordRoute;
-  "/users/register": typeof UsersRegisterRoute;
+  '/': typeof IndexRoute
+  '/administrador': typeof AdministradorRoute
+  '/cadastro': typeof CadastroRoute
+  '/cadastro-empresa': typeof CadastroEmpresaRoute
+  '/candidato': typeof CandidatoRoute
+  '/empresa': typeof EmpresaRoute
+  '/forgot-password': typeof ForgotPasswordRoute
+  '/login': typeof LoginRoute
+  '/reset-password': typeof ResetPasswordRoute
+  '/users/register': typeof UsersRegisterRoute
 }
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/administrador": typeof AdministradorRoute;
-  "/cadastro": typeof CadastroRoute;
-  "/cadastro-empresa": typeof CadastroEmpresaRoute;
-  "/candidato": typeof CandidatoRoute;
-  "/empresa": typeof EmpresaRoute;
-  "/forgot-password": typeof ForgotPasswordRoute;
-  "/login": typeof LoginRoute;
-  "/reset-password": typeof ResetPasswordRoute;
-  "/users/register": typeof UsersRegisterRoute;
+  '/': typeof IndexRoute
+  '/administrador': typeof AdministradorRoute
+  '/cadastro': typeof CadastroRoute
+  '/cadastro-empresa': typeof CadastroEmpresaRoute
+  '/candidato': typeof CandidatoRoute
+  '/empresa': typeof EmpresaRoute
+  '/forgot-password': typeof ForgotPasswordRoute
+  '/login': typeof LoginRoute
+  '/reset-password': typeof ResetPasswordRoute
+  '/users/register': typeof UsersRegisterRoute
 }
 export interface FileRoutesById {
-  __root__: typeof rootRouteImport;
-  "/": typeof IndexRoute;
-  "/administrador": typeof AdministradorRoute;
-  "/cadastro": typeof CadastroRoute;
-  "/cadastro-empresa": typeof CadastroEmpresaRoute;
-  "/candidato": typeof CandidatoRoute;
-  "/empresa": typeof EmpresaRoute;
-  "/forgot-password": typeof ForgotPasswordRoute;
-  "/login": typeof LoginRoute;
-  "/reset-password": typeof ResetPasswordRoute;
-  "/users/register": typeof UsersRegisterRoute;
+  __root__: typeof rootRouteImport
+  '/': typeof IndexRoute
+  '/administrador': typeof AdministradorRoute
+  '/cadastro': typeof CadastroRoute
+  '/cadastro-empresa': typeof CadastroEmpresaRoute
+  '/candidato': typeof CandidatoRoute
+  '/empresa': typeof EmpresaRoute
+  '/forgot-password': typeof ForgotPasswordRoute
+  '/login': typeof LoginRoute
+  '/reset-password': typeof ResetPasswordRoute
+  '/users/register': typeof UsersRegisterRoute
 }
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
+  fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
-    | "/"
-    | "/administrador"
-    | "/cadastro"
-    | "/cadastro-empresa"
-    | "/candidato"
-    | "/empresa"
-    | "/forgot-password"
-    | "/login"
-    | "/reset-password"
-    | "/users/register";
-  fileRoutesByTo: FileRoutesByTo;
+    | '/'
+    | '/administrador'
+    | '/cadastro'
+    | '/cadastro-empresa'
+    | '/candidato'
+    | '/empresa'
+    | '/forgot-password'
+    | '/login'
+    | '/reset-password'
+    | '/users/register'
+  fileRoutesByTo: FileRoutesByTo
   to:
-    | "/"
-    | "/administrador"
-    | "/cadastro"
-    | "/cadastro-empresa"
-    | "/candidato"
-    | "/empresa"
-    | "/forgot-password"
-    | "/login"
-    | "/reset-password"
-    | "/users/register";
+    | '/'
+    | '/administrador'
+    | '/cadastro'
+    | '/cadastro-empresa'
+    | '/candidato'
+    | '/empresa'
+    | '/forgot-password'
+    | '/login'
+    | '/reset-password'
+    | '/users/register'
   id:
-    | "__root__"
-    | "/"
-    | "/administrador"
-    | "/cadastro"
-    | "/cadastro-empresa"
-    | "/candidato"
-    | "/empresa"
-    | "/forgot-password"
-    | "/login"
-    | "/reset-password"
-    | "/users/register";
-  fileRoutesById: FileRoutesById;
+    | '__root__'
+    | '/'
+    | '/administrador'
+    | '/cadastro'
+    | '/cadastro-empresa'
+    | '/candidato'
+    | '/empresa'
+    | '/forgot-password'
+    | '/login'
+    | '/reset-password'
+    | '/users/register'
+  fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  AdministradorRoute: typeof AdministradorRoute;
-  CadastroRoute: typeof CadastroRoute;
-  CadastroEmpresaRoute: typeof CadastroEmpresaRoute;
-  CandidatoRoute: typeof CandidatoRoute;
-  EmpresaRoute: typeof EmpresaRoute;
-  ForgotPasswordRoute: typeof ForgotPasswordRoute;
-  LoginRoute: typeof LoginRoute;
-  ResetPasswordRoute: typeof ResetPasswordRoute;
-  UsersRegisterRoute: typeof UsersRegisterRoute;
+  IndexRoute: typeof IndexRoute
+  AdministradorRoute: typeof AdministradorRoute
+  CadastroRoute: typeof CadastroRoute
+  CadastroEmpresaRoute: typeof CadastroEmpresaRoute
+  CandidatoRoute: typeof CandidatoRoute
+  EmpresaRoute: typeof EmpresaRoute
+  ForgotPasswordRoute: typeof ForgotPasswordRoute
+  LoginRoute: typeof LoginRoute
+  ResetPasswordRoute: typeof ResetPasswordRoute
+  UsersRegisterRoute: typeof UsersRegisterRoute
 }
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/administrador": {
-      id: "/administrador";
-      path: "/administrador";
-      fullPath: "/administrador";
-      preLoaderRoute: typeof AdministradorRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/cadastro": {
-      id: "/cadastro";
-      path: "/cadastro";
-      fullPath: "/cadastro";
-      preLoaderRoute: typeof CadastroRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/cadastro-empresa": {
-      id: "/cadastro-empresa";
-      path: "/cadastro-empresa";
-      fullPath: "/cadastro-empresa";
-      preLoaderRoute: typeof CadastroEmpresaRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/candidato": {
-      id: "/candidato";
-      path: "/candidato";
-      fullPath: "/candidato";
-      preLoaderRoute: typeof CandidatoRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/empresa": {
-      id: "/empresa";
-      path: "/empresa";
-      fullPath: "/empresa";
-      preLoaderRoute: typeof EmpresaRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/forgot-password": {
-      id: "/forgot-password";
-      path: "/forgot-password";
-      fullPath: "/forgot-password";
-      preLoaderRoute: typeof ForgotPasswordRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/login": {
-      id: "/login";
-      path: "/login";
-      fullPath: "/login";
-      preLoaderRoute: typeof LoginRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/reset-password": {
-      id: "/reset-password";
-      path: "/reset-password";
-      fullPath: "/reset-password";
-      preLoaderRoute: typeof ResetPasswordRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
-    "/users/register": {
-      id: "/users/register";
-      path: "/users/register";
-      fullPath: "/users/register";
-      preLoaderRoute: typeof UsersRegisterRouteImport;
-      parentRoute: typeof rootRouteImport;
-    };
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/administrador': {
+      id: '/administrador'
+      path: '/administrador'
+      fullPath: '/administrador'
+      preLoaderRoute: typeof AdministradorRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/cadastro': {
+      id: '/cadastro'
+      path: '/cadastro'
+      fullPath: '/cadastro'
+      preLoaderRoute: typeof CadastroRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/cadastro-empresa': {
+      id: '/cadastro-empresa'
+      path: '/cadastro-empresa'
+      fullPath: '/cadastro-empresa'
+      preLoaderRoute: typeof CadastroEmpresaRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/candidato': {
+      id: '/candidato'
+      path: '/candidato'
+      fullPath: '/candidato'
+      preLoaderRoute: typeof CandidatoRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/empresa': {
+      id: '/empresa'
+      path: '/empresa'
+      fullPath: '/empresa'
+      preLoaderRoute: typeof EmpresaRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/forgot-password': {
+      id: '/forgot-password'
+      path: '/forgot-password'
+      fullPath: '/forgot-password'
+      preLoaderRoute: typeof ForgotPasswordRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/login': {
+      id: '/login'
+      path: '/login'
+      fullPath: '/login'
+      preLoaderRoute: typeof LoginRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/reset-password': {
+      id: '/reset-password'
+      path: '/reset-password'
+      fullPath: '/reset-password'
+      preLoaderRoute: typeof ResetPasswordRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/users/register': {
+      id: '/users/register'
+      path: '/users/register'
+      fullPath: '/users/register'
+      preLoaderRoute: typeof UsersRegisterRouteImport
+      parentRoute: typeof rootRouteImport
+    }
   }
 }
 
@@ -246,7 +246,7 @@ const rootRouteChildren: RootRouteChildren = {
   LoginRoute: LoginRoute,
   ResetPasswordRoute: ResetPasswordRoute,
   UsersRegisterRoute: UsersRegisterRoute,
-};
+}
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
+  ._addFileTypes<FileRouteTypes>()
